effc18860db0345559f67e1e988656d50b0853de
sshahram <shirin.shahram23@gmail.com>
2021-05-15 10:46:07 -0700

    add page routes and express-handlebars to server

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+const path = require('path');
 const express = require('express');
 const sequelize = require('./config/connection');
 const routes = require('./controllers');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create();
 require("dotenv").config();
 // session dependencies
 const session = require('express-session');
@@ -21,9 +24,14 @@ const sess = {
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(session(sess));
 // turn on routes
 app.use(routes);
+
+// app's template engine of choice
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on connection to db and server
 sequelize.sync({force: false}).then(() => {
