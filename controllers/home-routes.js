fdc1317b4a35191d59c1d6408c04be9d438ab5b9
sshahram <shirin.shahram23@gmail.com>
2021-05-15 10:16:10 -0700

    add route for single journal page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Journal, User, Answer} = require('../models');
+const { Journal, User, Answer, Mood} = require('../models');
 
 // route for login page
 router.get('/login', (req, res) => {
@@ -10,3 +10,54 @@ router.get('/login', (req, res) => {
     }
     res.render('login');
 });
+
+// route for single journal
+router.get('/journal/:id', (req, res) => {
+    Journal.findOne({
+        where: {
+            journalID: req.params.id,
+            userID: req.session.user_id
+        },
+        attributes: [
+            'journalID',
+            'journalNote',
+            'userID',
+            'moodID',
+            'createdAt',
+            'updatedAt',
+            'date',
+            'photoUrl'
+        ],
+        include: [
+            {
+                model: Mood,
+                attributes: ['moodID', 'title', 'color'],
+            },
+            {
+                model: User,
+                attributes: ['userName']
+            }
+        ]
+    })
+        .then(dbJournalData => {
+            if (!dbJournalData) {
+                res.status(404).json({ message: 'No journal found with this id' });
+                return;
+            }
+
+            // serialize the data
+            const journal = dbJournalData.get({ plain: true });
+
+            // pass data to template
+            res.render('single-journal', {
+                journal,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
