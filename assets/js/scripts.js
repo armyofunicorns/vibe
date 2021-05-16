c895ce112d6be1dbb3fe1e3a264e473452e149b6
Anthony Hall <anthony.w.hall@gmail.com>
2021-05-16 12:52:45 -0700

    Add modal

---
+++ assets/js/scripts.js
@@ -0,0 +1,9 @@
+// Functions for controlling the modal 
+function on() {
+    document.getElementById("modalUi").style.display = "block";
+}
+
+function off() {
+    document.getElementById("modalUi").style.display = "none";
+}
+
