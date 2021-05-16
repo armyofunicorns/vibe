0d89891b0561501f0a75a8baf9dc1729989bc40e
Stubblefish <george.l.wise@gmail.com>
2021-05-16 11:26:22 -0700

    added styling, date formatting function

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,63 +1,68 @@
-const router = require('express').Router();
-const { User } = require('../../models');
+const router = require("express").Router();
+const { User } = require("../../models");
+const path = require("path");
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../index.html"));
+  // res.send({ "hello world": "hello" });
+});
 
-router.post('/', (req, res) => {
-    User.create({
-        userName: req.body.userName,
-        password: req.body.password
+router.post("/", (req, res) => {
+  User.create({
+    userName: req.body.userName,
+    password: req.body.password,
+  })
+    .then((dbUserData) => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.userID;
+        req.session.userName = dbUserData.userName;
+        req.session.loggedIn = true;
+        const userData = {
+          user_id: dbUserData.userID,
+          userName: dbUserData.userName,
+          createdAt: dbUserData.createdAt,
+        };
+        res.json(userData);
+      });
     })
-    .then(dbUserData => {
-        req.session.save(() => {
-            req.session.user_id = dbUserData.userID;
-            req.session.userName = dbUserData.userName;
-            req.session.loggedIn = true;
-            const userData = {
-                'user_id': dbUserData.userID,
-                'userName': dbUserData.userName,
-                'createdAt': dbUserData.createdAt
-            }
-            res.json(userData);
-        });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({ message:'Failed to create a user!' });
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "Failed to create a user!" });
     });
 });
 
-router.post('/login', (req, res) => {
-    User.findOne({
-        where: {
-            userName: req.body.userName
-        }
-    })
-    .then(dbUserData => {
-        if (!dbUserData) {
-            res.status(404).json({ message: 'No user found with this username!'});
-            return;
-        }
-        const validPassword = dbUserData.checkPassword(req.body.password);
-        if (!validPassword) {
-            res.status(400).json({ message: 'Incorrect Password!'});
-            return;
-        }
-        req.session.save(() => {
-            req.session.user_id = dbUserData.userID;
-            req.session.userName = dbUserData.userName;
-            req.session.loggedIn = true;
-            const userData = {
-                'user_id': dbUserData.userID,
-                'userName': dbUserData.userName,
-                'createdAt': dbUserData.createdAt
-            }
+router.post("/login", (req, res) => {
+  User.findOne({
+    where: {
+      userName: req.body.userName,
+    },
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this username!" });
+        return;
+      }
+      const validPassword = dbUserData.checkPassword(req.body.password);
+      if (!validPassword) {
+        res.status(400).json({ message: "Incorrect Password!" });
+        return;
+      }
+      req.session.save(() => {
+        req.session.user_id = dbUserData.userID;
+        req.session.userName = dbUserData.userName;
+        req.session.loggedIn = true;
+        const userData = {
+          user_id: dbUserData.userID,
+          userName: dbUserData.userName,
+          createdAt: dbUserData.createdAt,
+        };
 
-            res.json({ user: userData, message: 'You are now logged in!'});
-        })
+        res.json({ user: userData, message: "You are now logged in!" });
+      });
     })
-    .catch(err => {
-        console.error(err);
-        res.status(500).json({ message: 'Failed to login!'})
-    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ message: "Failed to login!" });
+    });
 });
 
 module.exports = router;
