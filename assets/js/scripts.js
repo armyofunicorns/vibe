6842708c5e3cfb3cb175147da34129072e7778d2
Anthony Hall <anthony.w.hall@gmail.com>
2021-05-16 14:28:28 -0700

    Got the styling of the detail Day In Review page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,28 @@
+// Mood choice array
+// Array that stores markers and content 
+let moodChoices = [
+    [
+        'Upset', 1, './assets/images/mood-1-v4.svg','--quinary-color'
+    ],
+    [
+        'Sad', 2, './assets/images/mood-2-v4.svg','--quaternary-color'
+    ],
+    [
+        'Content', 3, './assets/images/mood-3-v4.svg','--tertiary-color'
+    ],
+    [
+        'Good', 4, './assets/images/mood-4-v4.svg',' --secondary-color'
+    ],
+    [
+        'Amazing', 5, './assets/images/mood-5-v4.svg','--primary-color'
+    ], 
+]; 
+
+// Control the background color of the page
+function changePageBackColor(color){
+    document.body.style.background = 'var(' + color + ')';
+}
+
 // Functions for controlling the modal 
 function on() {
     document.getElementById("modalUi").style.display = "block";
@@ -6,4 +31,5 @@ function on() {
 function off() {
     document.getElementById("modalUi").style.display = "none";
 }
+
 
