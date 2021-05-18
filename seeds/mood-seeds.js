98914787ccdfe06e0b45aa3875243d5bba2290a8
Stubblefish <george.l.wise@gmail.com>
2021-05-17 21:50:42 -0700

    Calendar!

--- seeds/mood-seeds.js
+++ seeds/mood-seeds.js
@@ -1,30 +1,26 @@
-const { Mood } = require('../models');
+const { Mood } = require("../models");
 
 const moodData = [
   {
-    title: 'joyful, happy, relaxed',
-    color: 'pink',
+    title: "productive, energetic, active",
+    color: "green",
   },
   {
-    title: 'sad, lonely, depressed',
-    color: 'blue',
+    title: "sad, lonely, depressed",
+    color: "blue",
   },
   {
-    title: 'productive, energetic, active',
-    color: 'green',
+    title: "sick, tired, lazy",
+    color: "yellow",
   },
   {
-    title: 'sick, tired, lazy',
-    color: 'yellow',
+    title: "average, normal, good",
+    color: "orange",
   },
   {
-    title: 'average, normal, good',
-    color: 'orange',
+    title: "angry, anxious, frustrated",
+    color: "red",
   },
-  {
-    title: 'angry, anxious, frustrated',
-    color: 'red',
-  }
 ];
 
 const seedMood = () => Mood.bulkCreate(moodData);
