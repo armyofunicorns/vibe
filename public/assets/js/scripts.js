0936e160f85876b743154d9eb0103bebb0901f74
jypara-git <zhyparaabdieva@gmail.com>
2021-05-18 17:52:02 -0700

    bug fix: remove hard coded date

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -56,7 +56,7 @@ async function submitJournal() {
         photoID: imgId,
         moodID: parseInt(selectedMood),
         journalNote: journalText,
-        date: "2021-05-12",
+        date: date,
     }
 
     const resp = await fetch("/api/journals", {
